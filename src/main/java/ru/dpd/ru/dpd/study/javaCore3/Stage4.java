65c89f71c4ee17fd468ec3d36c06d9a38d145c7a
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-01-30 11:04:54 +0500

    Доработка по результатам ревью шаг 3. Import * наше все.

--- src/main/java/ru/dpd/ru/dpd/study/javaCore3/Stage4.java
+++ src/main/java/ru/dpd/ru/dpd/study/javaCore3/Stage4.java
@@ -1,6 +1,12 @@
 package ru.dpd.ru.dpd.study.javaCore3;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /**
