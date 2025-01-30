e92f5fdda217d27b617421d31a87b3ae1ed23b3e
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-01-30 19:24:17 +0500

    Бонусное задание 0 по Java Core

---
+++ src/main/java/ru/dpd/javaCore/Runner.java
@@ -0,0 +1,9 @@
+package ru.dpd.javaCore;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        IntListStatistic.analizeList(Arrays.asList(1, -2, 0, 4, -5));
+    }
+}
